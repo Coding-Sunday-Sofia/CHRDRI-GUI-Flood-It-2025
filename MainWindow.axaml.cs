78c2097cbeee96e2028a27610a4c9f404f8b036d
Todor Balabanov <todor.balabanov@gmail.com>
2025-08-15 10:59:07 +0300

    Initial revision.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CHRDRI_GUI_Flood_It_2025;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
